6915f101984358022e5c8dd48975c7f9034b3ddd
Mike Liang <liang.mike.to@gmail.com>
2022-08-23 11:54:48 -0400

    update

--- main.js
+++ main.js
@@ -19,18 +19,34 @@ const scenes = [
   { // scene 3
     "isImageOrText": "text",
     "image": ``,
-    "button": `<button id="scene-3-button" onclick="scene3Button()" class="hover:text-sky-800"
+    "button": `<button id="scene-3-button" onclick="scene3Button()" class="hover:text-sky-800 pb-12"
     style="font-size: 24px; font-family: 'Times New Roman', Times, serif;">interesting...but how?</button>`,
     "caption": ``,
     "text": `
     <div class="prose text-left pt-24 pb-6">
-   
+    
     <h1 id="was-that-free-will-">Was that Free Will?</h1>
-<p>Most likely not...</p>
-<p>I think most people would believe that they chose it because they <em>created</em> the choice of clicking left or right, but I would argue the circumstance provided you the choice to click left or right.</p>
-<p>Since you clicked the &#39;Begin&#39; button, the next action arose from the consequences of that action there. <em>if that makes sense</em></p>
-<h4 id="so-what-influenced-my-belief-in-determinism-">So, what influenced my belief in determinism?</h4>
-<p>(culture, for one)</p>
+    <p>Most likely not...</p>
+    <p>I think most people would believe that they chose it because they <em>created</em> the choice of clicking left or right, but I would argue the circumstance provided you the choice to click left or right.</p>
+    <p>My belief is that all actions are a part of an unbreakable chain of consequences, whether good or bad. How did I come to this belief? In part because of culture and also through the ideas of Humes, that liberty is defined as the will to act or not act. From this I argue my point that circumstances give us the choices within the boundaries of the consequences from our previous actions, and so we only have the agency to act upon the choices, not create new choices -- which ultimately would be free will.</p>
+    <p><strong>tl;dr</strong></p>
+    <ul>
+    <li>choices arise from the circumstance</li>
+    <li>these choices lie within the boundaries of the consequences from our previous choice</li>
+    <li>thus, we have the will to choose, but not create the choice, which Hobbes defines as free will</li>
+    </ul>
+    <h3 id="storytime-">Storytime!</h3>
+    <p>Because of this worldview, combined with a sprinkle of other beliefs, I generally view actions that aren&#39;t favourable to my goals as a bad choice on my part. Likewise, I can choose better choices by putting myself in more favourable circumstances that give rise to better choices.</p>
+    <p>So when I decided to enroll in both HSP (intro to anthro, psych, and sociology) and Philosophy, I thought I made a bad choice. Why? Well, </p>
+    <ol>
+    <li>Less time in the summer to do other things</li>
+    <li>Are these 2 credits even worth that much?</li>
+    <li>Writing isn&#39;t my favourite thing to do</li>
+    </ol>
+    <p>But, because I believed that I can put myself in better circumstances by choosing to improve my writing and put out good quality work, I stuck with it. Now, I&#39;m pretty confident and love learning more about philosophy! :D</p>
+    <h4 id="so-what-influenced-my-belief-in-determinism-and-to-some-extent-compatibilism-">So, what influenced my belief in determinism, and to some extent, compatibilism?</h4>
+    <p>(culture, for one)</p>
+
 
 
     </div>
@@ -78,7 +94,7 @@ const scenes = [
   },
   { // scene 8
     "isImageOrText": "image",
-    "image": `<img src="images/thinker.jpg" width="40%" alt="">`,
+    "image": `<img src="images/thinker.png" width="40%" alt="">`,
     "button": `<button id="scene-8-button" onclick="scene8Button()" class="hover:text-sky-800 pt-6"
     style="font-size: 24px; font-family: 'Times New Roman', Times, serif;">next</button>`,
     "caption": `<h1 class="pt-12 pb-2 text-2xl"><i>cogito, ergo sum</i></h1>`,
